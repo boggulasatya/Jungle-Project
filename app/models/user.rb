de05d9117e770efe4ba25c9c25cbd4bc8a3c715a
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-11-09 20:08:18 -0600

    add tests for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  before_save { email.downcase! }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  
+
+  def self.authenticate_with_credentials(email, password)
+    email.downcase!
+    email.strip!
+    @user = User.find_by_email(email)
+
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
